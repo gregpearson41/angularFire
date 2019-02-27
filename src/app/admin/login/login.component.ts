3ea5d76695435c2e7a9f94e44c1ebb2da081c7bc
Greg <pearsong35@gmail.com>
2019-02-26 21:31:13 -0500

    updated files

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService} from '../../auth/auth.service.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private  authService:  AuthService) { }
   ngOnInit() {
   }
 }
-
